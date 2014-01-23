d17e0c210bbbfc1d7c939cf00836e1bda0ad6fb6
ttrfstud <ttrfstud@gmail.com>
2014-01-24 00:00:45 +0300

    in progress

--- antibiotics-sequencing/cyclopeptide-sequencing.js
+++ antibiotics-sequencing/cyclopeptide-sequencing.js
@@ -14,25 +14,33 @@ read('imt2', {encoding: 'utf8'}, function (e, d) {
 		var stack = [];
 		stack.push([0]);
 
+
 		var result = [];
 
-		console.log(stack);
-
 		while(stack.length) {
+			console.log('`````````````````````````');
+			console.log(JSON.stringify(spectrum), 'spectrum');
 			expand(stack);
+			if (stack.length <= 180) console.log(JSON.stringify(stack), 'bifo');
 
-			for (var i in stack) {
-				console.log(JSON.stringify(stack));
+			// console.log(JSON.stringify(stack));
 
+			for (var i = 0; i < stack.length; i++) {
 				if (cyclospectrumIs(stack[i], spectrum)) {
 					result.push(stack[i]);
-					stack = stack.splice(i, 1);
+					stack.splice(i, 1);
+					i--;
+					continue;
 				}
 
 				if (!consistent(stack[i], spectrum)) {
-					stack = stack.splice(i, 1);
+					stack.splice(i, 1);
+					i--;
 				}
+
 			}
+			if (stack.length <= 180) console.log(JSON.stringify(stack), 'afta');
+			console.log('==============================');
 		}
 
 		function out(el) {
@@ -50,18 +58,105 @@ function comp(a, b) {
 /* Never modify a spectrum under consideration ! */
 function cyclospectrumIs(testee, sp) {
 	testee = testee.slice(0);
+	sp = sp.slice(0);
 
-	testee.sort(comp);
+	var testeeSpectrum = toSpectrum(toSubpeptides(testee));
 
-	if (sp.length !== testee.length) {
+	if (testeeSpectrum.length !== sp.length) {
 		return false;
 	}
 
 	for (var i = 0; i < sp.length; i++) {
-		if (sp[i] !== testee[i]) return false;
+		var idx = sp.indexOf(testeeSpectrum[i]);
+
+		if (idx === -1) {
+			return false;
+		} else {
+			sp.splice(idx, 1);
+		}
+	}
+	console.log('TRU');
+
+	return true;
+}
+
+function toSpectrum(subpeptides) {
+
+	var noDuplicates = [];
+
+	// console.log('))))))))))))))))))))))))))))))))))))))toSpectrum');
+	console.log(JSON.stringify(subpeptides));
+
+	for (var i = 0; i < subpeptides.length; i++) {
+		if (!contains(noDuplicates, subpeptides[i])) {
+			noDuplicates.push(subpeptides[i]);
+		}
+	}
+
+	// console.log('noDuplicates', JSON.stringify(noDuplicates));
+
+	var spectrum = [];
+
+	for (var i = 0; i < noDuplicates.length; i++) {
+		spectrum.push(mass(noDuplicates[i]));	
+	}
+
+	spectrum.sort(comp);
+
+	// console.log(JSON.stringify(spectrum));
+	return spectrum;
+}
+
+function contains(subpeptides, subpeptide) {
+	for (var i = 0; i < subpeptides.length; i++) {
+		if (same(subpeptides[i], subpeptide)) return true;
+	}
+
+	return false;
+}
+
+function same(a1, a2) {
+	a1.sort(comp);
+	a2.sort(comp);
+
+	if (a1.length !== a2.length) {
+		return false;
+	}
+
+	for (var i = 0; i < a1.length; i++) {
+		if (a1[i] !== a2[i]) return false;
 	}
 
 	return true;
+}
+
+function toSubpeptides(peptide) {
+	// console.log(';;;;;;;;;;;;;;;;;;;;;;;;;;;;toSubpeptides');
+	// console.log(JSON.stringify(peptide));
+	var subpeptides = [[mass(peptide)]];
+
+	peptide = peptide.slice(1); // deleting 0, temporarily;
+
+	for (var i = 1; i < peptide.length; i++) {
+		for (var j = 0; j < peptide.length; j++) {
+			var subpeptide = peptide.slice(j, j + i);
+
+			// console.log(subpeptide);
+
+			if (subpeptide.length !== i) {
+				var diff = i - subpeptide.length;
+				subpeptide = subpeptide.concat(peptide.slice(0, diff));
+			}
+
+			subpeptides.push(subpeptide);
+		}
+	}
+
+	subpeptides.push([0]);
+
+	// console.log(JSON.stringify(subpeptides));
+	// console.log('::::::::::::::::::::::::::::');
+	return subpeptides;
 }
 
 function consistent(testee, sp) {
@@ -102,5 +197,19 @@ function expand(stack) {
 		}	
 	}
 
-	[].splice.apply(stack, [0, stack.length].concat(newStack));
+	stack.length = 0;
+
+	for  (var i = 0; i < newStack.length; i++) {
+		stack.push(newStack[i]);
+	}
+}
+
+function mass(peptide) {
+	var m = 0;
+
+	for (var i = 0; i < peptide.length; i++) {
+		m += peptide[i];
+	}
+
+	return m;
 }
